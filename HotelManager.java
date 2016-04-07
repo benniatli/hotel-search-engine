e25a24e85524aa5db7b094632c04d6849688881f
ingip <ipe1@hi.is>
2016-04-07 16:47:18 +0000

    ui

--- HotelManager.java
+++ HotelManager.java
@@ -2,20 +2,22 @@
 public class HotelManager {
 
 	
-	public static Hotel[] searchHotel(String date, int nrOfNights, String Loc, String name,
+	public Hotel[] searchHotel(String date, int nrOfNights, String Loc, String name,
 									int stars, int MaxPrice, int MinPrice){
-		Hotel[] foundHotels = DBManager.searchHotel(date, nrOfNights, Loc, name, stars, MaxPrice, MinPrice);
+		DBManager2 dbsearch = new DBManager2();
+		Hotel[] foundHotels = dbsearch.searchHotel(date, nrOfNights, Loc, name, stars, MaxPrice, MinPrice);
 		return foundHotels;
 	}
 	
-	public static String bookHotel(Hotel hotel, String date, int nrOfNights, int roomtype, String bookingName){
-		String booking = DBManager.bookHotel(hotel, date, nrOfNights, roomtype, bookingName);
+	public String bookHotel(Hotel hotel, String date, int nrOfNights, int roomtype, String bookingName){
+		DBManager2 dbbook = new DBManager2();
+		String booking = dbbook.bookHotel(hotel, date, nrOfNights, roomtype, bookingName);
 		return booking;
 	}
 	
 	
 	// reviews = HotelManager.getReviews()
-	public static Review[] getReviews(){
+	public Review[] getReviews(){
 		//need to find reviews
 		Review[] reviews = new Review[1];
 		return reviews;
