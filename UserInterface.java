1118537db364afd238d3ef2a62e8de0ef9983672
Balli <baldur.yngvason@gmail.com>
2016-04-15 00:25:38 +0000

    ALLT KOMMENTAD

--- UserInterface.java
+++ UserInterface.java
@@ -4,6 +4,7 @@ import java.text.SimpleDateFormat;
 import java.util.Date;
 
 
+//Athuga, daemi um notkun er i main fallinu, thar eru leidbeiningar um notkun.
 class UserInterface {
 	// field, constructor, and 
 	// method declarations
@@ -20,8 +21,22 @@ class UserInterface {
 	int nrOfNights;
 	int nrOfPersons;
 	int[] roomAmounts;
-
-
+	//Fyrir:
+	//	date = "yyyymmdd" er dagsetningu komu, skal vera a arinu 2016. Dagsetning+fjoldi
+	//		gistinotta ma ekki fara fram yfir arid 2016.
+	//	nrOfNights er fjoldi gistinotta. Passa skal ad fara ekki yfir a arid 2017.
+	//	loc er stadsetning hotels. strengur. Ef loc er tomi strengurinn er ekki gerd krafa a stadsetningu
+	//	name er nafn hotels. Leitad er eftir strengjum sem likjast name. Ekki tharf ad skrifa allt nafnid.
+	//		ef name er tomi strengurinn er ekki gerd krafa a nafn hotels
+	//	stars er lagmarksfjoldi stjarna sem leitad er eftir. Ef stars er neikvaed tala er ekki gerd krafa
+	//		a neinn stjornufjolda
+	//	maxPrice er hamarksverd allrar bokunarinnar. Ef maxPrice er neikvaett er ekki gerd krafa a kostnad bokunar.
+	//	Fylkid roomAmounts segir til um hve morg herbergi
+	//		skal boka af hverri herbergistegund. Fyrsta stakid taknar fjolda einstaklingsherbergja,
+	//		annad stakid taknar fjolda 2-manna herbergja, thridja stakid fjolda 3-manna herbergja
+	//		og fjorda stakid fjolda 4-manna herbergja.
+	//Eftir:
+	//	Fallid skilar array af hotel objectum sem uppfylla leitarkrofurnar
 	public Hotel[] searchHotel(String date, int nrOfNights, String loc, String name, int stars,
 			int maxPrice, int[] roomAmounts) throws ParseException{
 		HotelManager manager = new HotelManager();
@@ -33,7 +48,19 @@ class UserInterface {
 	}
 
 	
-	
+	//Fyrir:
+	//	hotel er hotel Object. Faest ut fra search adferdinni.
+	//	date = "yyyymmdd" er dagsetningu komu, skal vera a arinu 2016. Dagsetning+fjoldi
+	//		gistinotta ma ekki fara fram yfir arid 2016.
+	//	nrOfNights er fjoldi gistinotta. Passa skal ad fara ekki yfir a arid 2017.
+	//	bookingName er nafn bokunar. MA EKKI VERA TOMI STRENGURINN.
+	// ATH ATH ATH, EKKI LATA BOOKINGNAME VERA TOMA STRENGINN, OK!!!!??
+	//	Fylkid roomAmounts segir til um hve morg herbergi
+	//		skal boka af hverri herbergistegund. Fyrsta stakid taknar fjolda einstaklingsherbergja,
+	//		annad stakid taknar fjolda 2-manna herbergja, thridja stakid fjolda 3-manna herbergja
+	//		og fjorda stakid fjolda 4-manna herbergja.
+	//Eftir:
+	//	Buid er ad boka hotelid hotel fyrir gefnu dagsetninguna.
 	public String bookHotel(Hotel hotel, String date, int nrOfNights, String bookingName, int[] roomAmounts) throws ParseException{
 		HotelManager manager = new HotelManager();
 
@@ -74,7 +101,8 @@ class UserInterface {
 		int[] arr = {1,1,0,0};
 		
 		//Leytum ad hoteli fyrir gefna upphafsdagsetningu, 
-		//dagsetning skal vera strengur a forminu "yyyymmdd".
+		//dagsetning skal vera strengur a forminu "yyyymmdd". Tharf ad vera dagsetning
+		//a arinu 2016 og dagsetning+nrOfNights ma ekki fara yfir arid 2016.
 		String date = "20160801";
 		
 		//Fjoldi notta sem skal gista
@@ -102,7 +130,8 @@ class UserInterface {
 		Hotel[] hotel = session.searchHotel(date, nrOfNights, hotelLocation, hotelName,minStars,MaxPrice,arr);
 		//String bookHotel(Hotel hotel, String date, int nrOfNights, String bookingName, int[] roomAmounts, int whichLine)
 		
-		//Booking name er nafn adila sem bokar. 
+		//Booking name er nafn adila sem bokar. MA EKKI VERA TOMI STRENGURINN!!!!!
+		//NENNI EKKI AD LAGA THAD THANNIG BARA EKKI LATA TOMA STRENGINN, takk takk.
 		String bookingName = "Balli";
 		//Bokum hotel sem er hluti af listanum sem search skilar. 
 		String a = session.bookHotel(hotel[0], date, nrOfNights, bookingName, arr);
